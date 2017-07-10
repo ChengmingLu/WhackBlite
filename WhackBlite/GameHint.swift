22ab9982328bc0af94b7a9ac9d702d3ccc835782
Chengming Lu <cluster626@gmail.com>
2017-07-09 22:39:05 -0400

    ready for release

--- WhackBlite/GameHint.swift
+++ WhackBlite/GameHint.swift
@@ -11,12 +11,12 @@ import GameplayKit
 
 class GameHint: SKScene {
     
-    var hintTitle: eCATextLayer = eCATextLayer()
+    //var hintTitle: eCATextLayer = eCATextLayer()
     var hint: eCATextLayer = eCATextLayer()
     
     override func didMove(to view: SKView) {
         self.backgroundColor = UIColor.black
-        let titleSize = CGSize(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height / 6)
+        /*let titleSize = CGSize(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height / 6)
         let titleOrigin = CGPoint(x: UIScreen.main.bounds.width / 2 - titleSize.width / 2, y: 0)
         
         hintTitle.frame = CGRect(origin: titleOrigin, size: titleSize)
@@ -32,10 +32,10 @@ class GameHint: SKScene {
         hintTitle.fontSize = UIScreen.main.bounds.width / 20
         hintTitle.string = NSLocalizedString("gameHintTitle", comment: "")
         
-        self.view?.layer.addSublayer(hintTitle)
+        self.view?.layer.addSublayer(hintTitle)*/
         
-        let hinteSize = CGSize(width: UIScreen.main.bounds.width * 3 / 4, height: UIScreen.main.bounds.height / 2)
-        let hintOrigin = CGPoint(x: UIScreen.main.bounds.width / 2 - hinteSize.width / 2, y: titleSize.height)
+        let hinteSize = CGSize(width: UIScreen.main.bounds.width * 3 / 4, height: UIScreen.main.bounds.height * 1.5)
+        let hintOrigin = CGPoint(x: UIScreen.main.bounds.width / 2 - hinteSize.width / 2, y: UIScreen.main.bounds.height / 2 - hinteSize.height / 1.4)
         
         hint.frame = CGRect(origin: hintOrigin, size: hinteSize)
         
@@ -44,6 +44,8 @@ class GameHint: SKScene {
         hint.alignmentMode = kCAAlignmentCenter
         hint.foregroundColor = UIColor.white.cgColor
         //font
+        let systemFont = UIFont.systemFont(ofSize: 0.0)
+        let fontStringRef = systemFont.fontName as CFString
         hint.font = fontStringRef
         hint.fontSize = UIScreen.main.bounds.width / 20
         hint.string = NSLocalizedString("gameHint", comment: "")
@@ -57,6 +59,8 @@ class GameHint: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        hint.removeFromSuperlayer()
+        //hintTitle.removeFromSuperlayer()
         let transition = SKTransition.crossFade(withDuration: 1.0)
         let nextScene = GameScene(size: scene!.size)
         nextScene.scaleMode = .aspectFill
